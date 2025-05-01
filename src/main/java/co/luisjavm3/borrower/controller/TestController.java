e206cfcb1f2ba538c2c1dc434b3558a9c4f87c1e
Luis J. Montes <luisjavm3@gmail.com>
2025-04-30 19:45:57 -0500

    add test controller

---
+++ src/main/java/co/luisjavm3/borrower/controller/TestController.java
@@ -0,0 +1,14 @@
+package co.luisjavm3.borrower.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+    @GetMapping
+    public String test() {
+        return "Borrower Service - OK";
+    }
+}
